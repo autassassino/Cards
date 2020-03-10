d8a06f151facc7b5ee14476f8c3a8c6bbb8ede67
julia <bgg242@myy.haaga-helia.fi>
2020-03-10 15:53:54 +0200

    modified:   list.php

--- list.php
+++ list.php
@@ -16,6 +16,7 @@ if($_SESSION['username'] == 'username') {
  echo $row["city"] . " ";
  echo $row["state"] . " ";
  echo $row["country"] . " ";
+ echo "<a href=\"delete.php?id=" . $row["id"] . "\">DELETE</a>";
  print("<br>");
  }
  } else {
@@ -42,7 +43,6 @@ if($_SESSION['username'] == 'username') {
  print("<br>");
  print("Country: ");
  print("<input type=\"text\" name=\"country\">");
- echo "<a href=\"delete.php?id=" . $row["id"] . "\">DELETE</a>"; 
  print("<br>");
  print("");
  print("<input type=\"submit\" name=\"submit\" value=\"Save\" >");
